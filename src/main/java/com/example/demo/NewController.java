6a3a1d6bd579257b3f7134b35866afaef284ebac
saravananramakrishnan <saravanan.1976@gmail.com>
2024-03-12 22:11:03 -0400

    minor changs

--- src/main/java/com/example/demo/NewController.java
+++ src/main/java/com/example/demo/NewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/hello")
     public String test() {
         System.out.println("Hello World");
         return "Hello world";
